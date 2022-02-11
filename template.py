a53e2d001a7fb0aac7cb4ffd7d5aab8f2e19efdd
dvor4x <dvor4x@hzv.fr>
2022-02-11 21:57:56 +0100

    version 1

---
+++ template.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import os
+from pwn import *
+
+BIN = "[CHALL_NAME"]
+HOST = "chall.remote.ctf"
+PORT = 42666
+USER = "USER"
+PASS = "PASS"
+LOCALBIN = os.getcwd() + "/" + BIN
+
+def exploit(r):
+    print("[+] IMPLEMENT ME")
+
+def starter():
+    r = ""
+    if (len(sys.argv) == 2):
+        if sys.argv[1] == "remote":
+            r = remote(HOST,PORT)
+        elif sys.argv[1] == "remote-ssh":
+            s = ssh(host=HOST, user=USER, password=PASS,port=PORT)
+            r = s.process(BIN)
+        else: # no debug
+            r = process(LOCALBIN)
+    else:
+        context.terminal = ["tmux", "splitw", "-h"]
+        r = process(LOCALBIN)
+        gdb.attach(r, """
+        break main
+        continue
+        """)
+    return r
+
+if __name__ == "__main__":
+    r = starter()
+    exploit(r)
